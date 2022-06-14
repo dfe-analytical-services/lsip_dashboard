8d6c856a76d793971777c0931107cfdac0123f3a
Paul James <paul.james@education.gov.uk>
2022-06-14 15:25:36 +0100

    Added new datasets: vacancy and acheivements Added replacement datasets: APS employment with added LAD and region Added code to tidy up new datasets and add on LEP to vacancy data Added code to output dataframes to xlxs

--- R/Loading Core Indicators.R
+++ R/Loading Core Indicators.R
@@ -1,8 +1,8 @@
 ####
 # Title: LSIP dashboard - proof of concept
-# Author: Hannah Cox
+# Author: Hannah Cox/Paul James
 # Date: 18th May 2022
-# Last updated: 8th June 2022
+# Last updated: 14th June 2022
 ###
 
 # Load libraries ----
@@ -23,23 +23,34 @@ C_LEP2020 <- I_LEP2020 %>%
   select(ERNM)
 
 ## APS ----
-### Employment by occupation ----
+### Core indicator 2: Employment by occupation ----
 # Download from https://www.nomisweb.co.uk/datasets/apsnew
 #Query data
-#Geography: England, LEPs (as of April 2021)
+#Geography: England, LEPs, regions, LADs (as of April 2021)
 #Date: 12 months to Dec 2017-2021
-#Cell: T09b Employment by occupation (SOC2010) sub-major group and full-time/part-time; All people/ All people
+#Cell: T09a Employment by occupation (SOC2010) sub-major group and full-time/part-time; All people/ All people
+I_EmpOcc_APS1721 <- read.xlsx(xlsxFile="./Data/nomis_2022_06_14_092401.xlsx",skipEmptyRows=T)
 
-I_EmpOcc_APS1721 <- read.xlsx(xlsxFile="./Data/nomis_2022_06_08_092847.xlsx", sheet=1, skipEmptyRows=T)
-
-### Employment level and rate ------------
+### Core indicator 5: Employment level and rate ------------
+#Geog and date as above
 #Cell: T01 Economic activity by age Aged 16-64/ All people
+I_EmpRate_APS1721 <- read.xlsx(xlsxFile="./Data/nomis_2022_06_14_095314.xlsx", sheet=1, skipEmptyRows=T)
 
-I_EmpRate_APS1721 <- read.xlsx(xlsxFile="./Data/nomis_2022_06_08_111841.xlsx", sheet=1, skipEmptyRows=T)
+##ILR
+### Core indicator 12: AY20/21 achievements by SSAt1 and LAD ------------
+## Permalink: https://explore-education-statistics.service.gov.uk/data-tables/permalink/c390bb3f-8577-40f1-869c-fc8a8195516e
+I_Achieve_ILR21 <- read.csv(file="./Data/permalink-c390bb3f-8577-40f1-869c-fc8a8195516e.csv")
 
+### Core indicator 13: AY16/17-20/21 achievements by LAD and provision------------
+## Permalink: https://explore-education-statistics.service.gov.uk/data-tables/permalink/3960ad0f-fd8a-49bb-91d7-f3ca1181b93f
+I_Achieve_ILR1621 <- read.csv(file="./Data/permalink-3960ad0f-fd8a-49bb-91d7-f3ca1181b93f.csv")
 
+##ONS
+### Core indicator 22: Vacancy by year and LAD ------------
+##Download link: https://www.ons.gov.uk/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/datasets/onlinejobadvertsbyitl1regionandlocalauthority
+I_Vacancy_ONS1722 <- read.xlsx(xlsxFile="./Data/referencetablesupdated.xlsx", sheet="1", skipEmptyRows=T,startRow=4)
 
-# Functions ----
+# Data cleaning functions ----
 format.EmpOcc.APS <- function(x) { # need to clean up colnames
   x %>% mutate(year = ifelse(annual.population.survey == "date", X2, NA))%>% # tag time periods
     fill(year)%>% # fill time periods for all rows
@@ -50,8 +61,10 @@ format.EmpOcc.APS <- function(x) { # need to clean up colnames
     filter(check ==1)%>%
     filter(!grepl("nomisweb", area))%>%
     select(year = jan_2017_dec_2017, area, everything(), - check) %>%# reorder and remove
+    mutate(geographic_level = gsub(":.*", "", area)) %>% # Get geog type
     mutate(area = gsub(".*:", "", area)) %>% # Tidy up Area names
-    mutate_at(c(3:27),as.numeric) # Convert to numeric
+    mutate_at(c(3:27),as.numeric)%>% # Convert to numeric
+    relocate(geographic_level, .after = area)
 }
 
 format.EmpRate.APS <- function(x) { # need to clean up colnames
@@ -63,19 +76,47 @@ format.EmpRate.APS <- function(x) { # need to clean up colnames
     mutate(check = ifelse(grepl(":", area), 1, 0))%>% # remove anything but LEP and Country
     filter(check ==1)%>%
     filter(!grepl("nomisweb", area))%>%
+    mutate(geographic_level = gsub(":.*", "", area)) %>% # Get geog type
     mutate(area = gsub(".*:", "", area))%>% # Tidy up Area names
     mutate_at(c(2:9),as.numeric)%>% # Convert to numeric
     select(year=x2017, area, everything(), - check)%>%# reorder and remove
-    mutate(empRate = .[[5]]/.[[3]])
+    mutate(empRate = .[[5]]/.[[3]])%>%
+    relocate(geographic_level, .after = area)
 }
 
-# Indicators ----
+#Clean ILR column names, reorder and reformat
+format.AchieveSSA.ILR <- function(x) { # need to clean up colnames
+  x %>% rename(area=ï..location)%>%
+    left_join(select(I_LEP2020,LACD,LEP=ERNM),by= c("location_code"="LACD"))%>%
+    relocate(LEP, .after = geographic_level)%>%
+    relocate(time_period, .before = area)%>%
+    rename_all(recode, e_and_t_aims_ach="achievements")%>%
+    mutate(achievements=as.numeric(achievements))
+}
+
+#Reshape vacancy data to long, rename and reorder and reformat some columns
+format.Vacancy.ONS <- function(x) { # need to clean up colnames
+  x %>% gather(year, vacancy_unit, 3:8)%>%
+    rename(LA="Local.authority.[note.1]",region="Region.[note.2]")%>%
+    left_join(select(I_LEP2020,LANM,LEP=ERNM),by= c("LA"="LANM"))%>%
+    relocate(LEP, .after = region)%>%
+    relocate(year,.before=LA)%>%
+    mutate(year=as.numeric(year))
+}
+
+# Clean indicators datasets----
 ## Employment by occupation ----
 C_EmpOcc_APS1721 <- format.EmpOcc.APS(I_EmpOcc_APS1721)
 
 ## Employment level and rate ----
 C_EmpRate_APS1721 <- format.EmpRate.APS(I_EmpRate_APS1721)
 
+##Achievements
+C_Achieve_ILR1621 <- format.AchieveSSA.ILR(I_Achieve_ILR1621)
+C_Achieve_ILR21 <- format.AchieveSSA.ILR(I_Achieve_ILR21)
+
+#vacancy
+C_Vacancy_ONS1722 <- format.Vacancy.ONS(I_Vacancy_ONS1722)
 
 # Save to SQL ----
 #con <- dbConnect(odbc(), Driver = "SQL Server Native Client 11.0", 
@@ -95,6 +136,10 @@ C_EmpRate_APS1721 <- format.EmpRate.APS(I_EmpRate_APS1721)
 #con %>% dbWriteTable("MA_UFS_S_DATA.LSIP.dashboard_data", X)
 
 # Combine into single workbook ----
-
-
+list_of_datasets <- list("2.Emp by occupation" = C_EmpOcc_APS1721,
+                        "5.Emp rate" = C_EmpRate_APS1721,
+                        "12.FE achievements SSA"=C_Achieve_ILR21,
+                        "x.FE achievements"=C_Achieve_ILR1621,
+                        "22.Vacancies"=C_Vacancy_ONS1722)
+write.xlsx(list_of_datasets, file = "202206CoreIndicators.xlsx")
 
