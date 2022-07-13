885eb5d110ced122be5b10f3384cb848c12d5b7a
Rich Bielby <richard.bielby@education.gov.uk>
2022-07-13 16:32:03 +0100

    Ran tidy_code().

--- tests/shinytest/initial_load_test.R
+++ tests/shinytest/initial_load_test.R
@@ -7,14 +7,14 @@ run_set_shinytests <- function(dfinputs, outstring, listrecords) {
     file <- paste0(outstring, "_", i - 1, ".json")
     eval(parse(text = paste0("app$setInputs(", dfinputs$field[i], '="', dfinputs$value[i], '", timeout_ = 1e+4, values_ = FALSE)')))
     app$snapshot(
-#      items = listrecords,
+      #      items = listrecords,
       filename = file
     )
-#    clean_json(file)
+    #    clean_json(file)
   }
 }
 
-app <- ShinyDriver$new("../../",loadTimeout=6.e4)
+app <- ShinyDriver$new("../../", loadTimeout = 6.e4)
 app$snapshotInit("initial_load_test", screenshot = FALSE)
 app$snapshot(
   filename = "LoadHome.json"
@@ -23,20 +23,20 @@ app$snapshot(
 
 dfTestInputs <- data.frame(
   field = c(
-    "navbar","lep0a",
-    "navbar","lep1","lep2",
-    "navbar","lep5","lep6",
-    "navbar","lep7","lep8",
-    "navbar","lep3","lep4",
-    "navbar","lep9","lep10"
+    "navbar", "lep0a",
+    "navbar", "lep1", "lep2",
+    "navbar", "lep5", "lep6",
+    "navbar", "lep7", "lep8",
+    "navbar", "lep3", "lep4",
+    "navbar", "lep9", "lep10"
   ),
   value = c(
-    "Overview","Cheshire and Warrington",
-    "Employment","London","Greater Manchester",
-    "Vacancies","London","Greater Manchester",
-    "Earnings","London","Greater Manchester",
-    "FE","London","Greater Manchester",
-    "HE","London","Greater Manchester"
+    "Overview", "Cheshire and Warrington",
+    "Employment", "London", "Greater Manchester",
+    "Vacancies", "London", "Greater Manchester",
+    "Earnings", "London", "Greater Manchester",
+    "FE", "London", "Greater Manchester",
+    "HE", "London", "Greater Manchester"
   )
 )
 run_set_shinytests(dfTestInputs, "localskills", "")
