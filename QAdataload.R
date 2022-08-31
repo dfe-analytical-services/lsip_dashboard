87b505809ad6c62f9cebc1243a953b73c1c4a7b2
JAMES <Paul.JAMES@EDUCATION.GOV.UK>
2022-08-31 15:56:16 +0100

    tidycode

--- QAdataload.R
+++ QAdataload.R
@@ -1,7 +1,7 @@
-#check the basic shape of updated files match previous files
+# check the basic shape of updated files match previous files
 # to be run after 1)ExtractLoadData.R and 2)TransformData.R and 3)R/Loading Core Indicators.R
 
-#Load in base versions of app file to compare against
+# Load in base versions of app file to compare against
 ## LEP data ----
 # list of LEPS
 C_LEP2020Base <- read.csv(file = "Data\\AppDataBaseForQA\\C_LEP2020.csv", check.names = FALSE)
@@ -40,40 +40,40 @@ C_Vacancy_EnglandBase <- read.csv(file = "Data\\AppDataBaseForQA\\C_Vacancy_Engl
 C_Vacancy_England_max_minBase <- read.csv(file = "Data\\AppDataBaseForQA\\C_Vacancy_England_max_min.csv", check.names = FALSE)
 C_Vacancy_England_changeBase <- read.csv(file = "Data\\AppDataBaseForQA\\C_Vacancy_England_change.csv", check.names = FALSE)
 
-#check there are 38 leps
-QA<-data.frame(QAcheck=nrow(C_LEP2020)==nrow(C_LEP2020Base))
-#Check data tables have same columns and class base case
-QA[2,1]<-identical(D_EmpOcc_APS1721[NA,][1,], D_EmpOcc_APS1721Base[NA,][1,])
-QA[3,1]<-identical(C_EmpOcc_APS1721[NA,][1,], C_EmpOcc_APS1721Base[NA,][1,])
-QA[4,1]<-identical(D_EmpRate_APS1721[NA,][1,], D_EmpRate_APS1721Base[NA,][1,])
-QA[5,1]<-identical(C_EmpRate_APS1721[NA,][1,], C_EmpRate_APS1721Base[NA,][1,])
-QA[6,1]<-identical(C_EmpRate_APS1721_max_min[NA,][1,], C_EmpRate_APS1721_max_minBase[NA,][1,])
-QA[7,1]<-identical(D_Achieve_ILR1621[NA,][1,], D_Achieve_ILR1621Base[NA,][1,])
-QA[8,1]<-identical(C_Achieve_ILR1621[NA,][1,], C_Achieve_ILR1621Base[NA,][1,])
-QA[9,1]<-identical(C_Achieve_ILR1621_max_min[NA,][1,], C_Achieve_ILR1621_max_minBase[NA,][1,])
-QA[10,1]<-identical(D_Achieve_ILR21[NA,][1,], D_Achieve_ILR21Base[NA,][1,])
-QA[11,1]<-identical(C_Achieve_ILR21[NA,][1,], C_Achieve_ILR21Base[NA,][1,])
-QA[12,1]<-identical(C_Vacancy_ONS1722[NA,][1,], C_Vacancy_ONS1722Base[NA,][1,])
-QA[13,1]<-identical(C_Vacancy_England[NA,][1,], C_Vacancy_EnglandBase[NA,][1,])
-QA[14,1]<-identical(C_Vacancy_England_max_min[NA,][1,], C_Vacancy_England_max_minBase[NA,][1,])
-QA[15,1]<-identical(C_Vacancy_England_change[NA,][1,], C_Vacancy_England_changeBase[NA,][1,])
+# check there are 38 leps
+QA <- data.frame(QAcheck = nrow(C_LEP2020) == nrow(C_LEP2020Base))
+# Check data tables have same columns and class base case
+QA[2, 1] <- identical(D_EmpOcc_APS1721[NA, ][1, ], D_EmpOcc_APS1721Base[NA, ][1, ])
+QA[3, 1] <- identical(C_EmpOcc_APS1721[NA, ][1, ], C_EmpOcc_APS1721Base[NA, ][1, ])
+QA[4, 1] <- identical(D_EmpRate_APS1721[NA, ][1, ], D_EmpRate_APS1721Base[NA, ][1, ])
+QA[5, 1] <- identical(C_EmpRate_APS1721[NA, ][1, ], C_EmpRate_APS1721Base[NA, ][1, ])
+QA[6, 1] <- identical(C_EmpRate_APS1721_max_min[NA, ][1, ], C_EmpRate_APS1721_max_minBase[NA, ][1, ])
+QA[7, 1] <- identical(D_Achieve_ILR1621[NA, ][1, ], D_Achieve_ILR1621Base[NA, ][1, ])
+QA[8, 1] <- identical(C_Achieve_ILR1621[NA, ][1, ], C_Achieve_ILR1621Base[NA, ][1, ])
+QA[9, 1] <- identical(C_Achieve_ILR1621_max_min[NA, ][1, ], C_Achieve_ILR1621_max_minBase[NA, ][1, ])
+QA[10, 1] <- identical(D_Achieve_ILR21[NA, ][1, ], D_Achieve_ILR21Base[NA, ][1, ])
+QA[11, 1] <- identical(C_Achieve_ILR21[NA, ][1, ], C_Achieve_ILR21Base[NA, ][1, ])
+QA[12, 1] <- identical(C_Vacancy_ONS1722[NA, ][1, ], C_Vacancy_ONS1722Base[NA, ][1, ])
+QA[13, 1] <- identical(C_Vacancy_England[NA, ][1, ], C_Vacancy_EnglandBase[NA, ][1, ])
+QA[14, 1] <- identical(C_Vacancy_England_max_min[NA, ][1, ], C_Vacancy_England_max_minBase[NA, ][1, ])
+QA[15, 1] <- identical(C_Vacancy_England_change[NA, ][1, ], C_Vacancy_England_changeBase[NA, ][1, ])
 
-#check they have the same key categories
-QA[16,1]<-identical(distinct(D_EmpOcc_APS1721,area,geographic_level), distinct(D_EmpOcc_APS1721Base,area,geographic_level))
-QA[17,1]<-identical(distinct(C_EmpOcc_APS1721,Occupation), distinct(C_EmpOcc_APS1721Base,Occupation))
-QA[18,1]<-identical(distinct(D_EmpRate_APS1721,area,geographic_level), distinct(D_EmpRate_APS1721,area,geographic_level))
-QA[19,1]<-identical(distinct(C_EmpRate_APS1721,area,geographic_level), distinct(C_EmpRate_APS1721,area,geographic_level))
-QA[20,1]<-identical(distinct(C_EmpRate_APS1721_max_min,area), distinct(C_EmpRate_APS1721_max_min,area))
-QA[21,1]<-identical(distinct(D_Achieve_ILR1621,area,location_code,geographic_level,LEP,age_group,level_or_type), distinct(D_Achieve_ILR1621,area,location_code,geographic_level,LEP,age_group,level_or_type))
-QA[22,1]<-identical(distinct(C_Achieve_ILR1621,LEP,level_or_type), distinct(C_Achieve_ILR1621,LEP,level_or_type))
-QA[23,1]<-identical(distinct(C_Achieve_ILR1621_max_min,LEP,level_or_type), distinct(C_Achieve_ILR1621_max_min,LEP,level_or_type))
-QA[24,1]<-identical(distinct(D_Achieve_ILR21,area,location_code,geographic_level,LEP,ssa_t1_desc), distinct(D_Achieve_ILR21,area,location_code,geographic_level,LEP,ssa_t1_desc))
-QA[25,1]<-identical(distinct(C_Achieve_ILR21,LEP,SSA), distinct(C_Achieve_ILR21,LEP,SSA))
-QA[26,1]<-identical(distinct(C_Vacancy_ONS1722,LA,region,LEP), distinct(C_Vacancy_ONS1722,LA,region,LEP))
-QA[27,1]<-identical(distinct(C_Vacancy_England,LEP), distinct(C_Vacancy_England,LEP))
-QA[28,1]<-identical(distinct(C_Vacancy_England_max_min,LEP), distinct(C_Vacancy_England_max_min,LEP))
-QA[29,1]<-identical(distinct(C_Vacancy_England_change,LEP), distinct(C_Vacancy_England_change,LEP))
+# check they have the same key categories
+QA[16, 1] <- identical(distinct(D_EmpOcc_APS1721, area, geographic_level), distinct(D_EmpOcc_APS1721Base, area, geographic_level))
+QA[17, 1] <- identical(distinct(C_EmpOcc_APS1721, Occupation), distinct(C_EmpOcc_APS1721Base, Occupation))
+QA[18, 1] <- identical(distinct(D_EmpRate_APS1721, area, geographic_level), distinct(D_EmpRate_APS1721, area, geographic_level))
+QA[19, 1] <- identical(distinct(C_EmpRate_APS1721, area, geographic_level), distinct(C_EmpRate_APS1721, area, geographic_level))
+QA[20, 1] <- identical(distinct(C_EmpRate_APS1721_max_min, area), distinct(C_EmpRate_APS1721_max_min, area))
+QA[21, 1] <- identical(distinct(D_Achieve_ILR1621, area, location_code, geographic_level, LEP, age_group, level_or_type), distinct(D_Achieve_ILR1621, area, location_code, geographic_level, LEP, age_group, level_or_type))
+QA[22, 1] <- identical(distinct(C_Achieve_ILR1621, LEP, level_or_type), distinct(C_Achieve_ILR1621, LEP, level_or_type))
+QA[23, 1] <- identical(distinct(C_Achieve_ILR1621_max_min, LEP, level_or_type), distinct(C_Achieve_ILR1621_max_min, LEP, level_or_type))
+QA[24, 1] <- identical(distinct(D_Achieve_ILR21, area, location_code, geographic_level, LEP, ssa_t1_desc), distinct(D_Achieve_ILR21, area, location_code, geographic_level, LEP, ssa_t1_desc))
+QA[25, 1] <- identical(distinct(C_Achieve_ILR21, LEP, SSA), distinct(C_Achieve_ILR21, LEP, SSA))
+QA[26, 1] <- identical(distinct(C_Vacancy_ONS1722, LA, region, LEP), distinct(C_Vacancy_ONS1722, LA, region, LEP))
+QA[27, 1] <- identical(distinct(C_Vacancy_England, LEP), distinct(C_Vacancy_England, LEP))
+QA[28, 1] <- identical(distinct(C_Vacancy_England_max_min, LEP), distinct(C_Vacancy_England_max_min, LEP))
+QA[29, 1] <- identical(distinct(C_Vacancy_England_change, LEP), distinct(C_Vacancy_England_change, LEP))
 
-#check for any errors
+# check for any errors
 print("If you have any errors they will be listed here:")
 which(QA == "FALSE", arr.ind = TRUE)
