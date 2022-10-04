7853190ffe976fd06abb32e376891232a1b3d104
JAMES <Paul.JAMES@EDUCATION.GOV.UK>
2022-10-04 16:23:50 +0100

    added in some missing p tags

--- ui.R
+++ ui.R
@@ -173,9 +173,10 @@ fluidPage(
       h1("Data sources"),
       fluidRow(style="padding-left:15px;padding-right:15px;",
         column(12,
-      fluidRow(dataTableOutput('DataTbl'))
-    )
-    ),
+      fluidRow(
+        column(12,
+               dataTableOutput('DataTbl'))
+      ),
     br(),
     fluidRow(
       column(12,
@@ -195,6 +196,8 @@ fluidPage(
                              The data is taken from the FE and Skills official statistics."),
         )
       )
+    )
+    )
     ),
     # APP ----
 
@@ -238,39 +241,37 @@ fluidPage(
     # Create the accessibility statement-----------------
     tabPanel(
       "Accessibility",
-      gov_main_layout(
-        gov_row(
+        fluidRow(style="padding-left:15px;padding-right:15px;",
           column(
             width = 12,
-            h2("Accessibility statement"),
-            br("This accessibility statement applies to the Local Skills dashboard.
+            h1("Accessibility statement"),
+            p("This accessibility statement applies to the Local Skills dashboard.
             This dashboard is run by the Department for Education. We want as many people as possible to be able to use this application,
             and have actively developed this dashboard with accessibilty in mind."),
-            h3("WCAG 2.1 compliance"),
-            br("We follow the reccomendations of the ", a(href = "https://www.w3.org/TR/WCAG21/", "WCAG 2.1 requirements. "), "This application has been checked using the ", a(href = "https://github.com/ewenme/shinya11y", "Shinya11y tool "), ", which did not detect accessibility issues.
+            h2("WCAG 2.1 compliance"),
+            p("We follow the reccomendations of the ", a(href = "https://www.w3.org/TR/WCAG21/", "WCAG 2.1 requirements. "), "This application has been checked using the ", a(href = "https://github.com/ewenme/shinya11y", "Shinya11y tool "), ", which did not detect accessibility issues.
              This dashboard also fully passes the accessibility audits checked by the ", a(href = "https://developers.google.com/web/tools/lighthouse", "Google Developer Lighthouse tool"), ". This means that this dashboard:"),
             tags$div(tags$ul(
               tags$li("uses colours that have sufficient contrast"),
               tags$li("allows you to zoom in up to 300% without the text spilling off the screen"),
               tags$li("has its performance regularly monitored, with a team working on any feedback to improve accessibility for all users")
             )),
-            h3("Limitations"),
-            br("We recognise that there are still potential issues with accessibility in this dashboard, but we will continue
+            h2("Limitations"),
+            p("We recognise that there are still potential issues with accessibility in this dashboard, but we will continue
              to review updates to technology available to us to keep improving accessibility for all of our users. For example, these
             are known issues that we will continue to monitor and improve:"),
             tags$div(tags$ul(
               tags$li("Keyboard navigation through the interactive charts is currently limited, and some features are unavailable for keyboard only users"),
               tags$li("Alternative text in interactive charts is limited to titles and could be more descriptive (although this data is available in csv format)")
             )),
-            h3("Feedback"),
-            br(
+            h2("Feedback"),
+            p(
               "If you have any feedback on how we could further improve the accessibility of this dashboard, please contact us at",
               a(href = "mailto:statistics.development@education.gov.uk", "statistics.development@education.gov.uk")
             ),
             br()
           )
         )
-      )
     ), # End of accessibility tab
     # Support links ===========================================================
 
