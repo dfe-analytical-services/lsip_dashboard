6b177e53532a5f5d527210f4fa41c3c471fc25a6
Rich Bielby <richard.bielby@education.gov.uk>
2022-10-18 16:36:52 +0100

    Adjusting header image size.

--- ui.R
+++ ui.R
@@ -142,7 +142,7 @@ padding-top:15px
     <div class="govuk-header__logo" style="width: 15%; margin-left: 15px;float:left;">
     <a href="https://www.gov.uk/government/organisations/department-for-education" class="govuk-header__link govuk-header__link--homepage">
     <span class="govuk-header__logotype">
-    <img src="images/DfE_logo_landscape.png" class="govuk-header__logotype-crown-fallback-image"/>
+    <img src="images/DfE_logo_landscape.png" height=32 width=146/>
     <span class="govuk-header__logotype-text"></span>
     </span>
     </a>
