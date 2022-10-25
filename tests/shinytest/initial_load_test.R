c5567555d0191a2a98af78514d23c7648be77ae2
Rich Bielby <richard.bielby@education.gov.uk>
2022-10-25 15:23:15 +0100

    extended shint test timeout

--- tests/shinytest/initial_load_test.R
+++ tests/shinytest/initial_load_test.R
@@ -6,7 +6,7 @@ run_set_shinytests <- function(dfinputs, outstring, listrecords) {
   for (i in 1:nrow(dfinputs)) {
     file <- paste0(outstring, "_", i - 1, ".json")
     cat(paste0(dfinputs$field[i], '="', dfinputs$value[i]), fill = TRUE)
-    eval(parse(text = paste0("app$setInputs(", dfinputs$field[i], '="', dfinputs$value[i], '", timeout_ = 1.2e+4, values_ = FALSE)')))
+    eval(parse(text = paste0("app$setInputs(", dfinputs$field[i], '="', dfinputs$value[i], '", timeout_ = 3.6e+4, values_ = FALSE)')))
     app$snapshot(
       #      items = listrecords,
       filename = file
