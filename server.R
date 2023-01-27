8ec15906469d2cb6b1f3e25190a196ef2d8b5443
JAMES <Paul.JAMES@EDUCATION.GOV.UK>
2023-01-27 11:07:51 +0000

    remove uneeded print

--- server.R
+++ server.R
@@ -2347,8 +2347,6 @@ server <- function(input, output, session) {
         Year = time_period, Geography = geographic_level, Area = area,
         Data=metric,Breakdown=breakdown,Splits=subgroups,Value=value
       ))
-    #  , escape = FALSE, options = list(dom = "t"), rownames = FALSE)
-    #print(input$hubLA)
   })
 
   # create hub code
