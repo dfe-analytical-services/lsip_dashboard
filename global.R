3f49d86cda65eafccb0987b70c856e337e3d24df
JAMES <Paul.JAMES@EDUCATION.GOV.UK>
2023-02-03 11:54:31 +0000

    add in level 3 qualified data

--- global.R
+++ global.R
@@ -56,7 +56,8 @@ metricChoices <- list(
     "FE start rate" = "starts_rate_per_100000_population",
     "FE achievements" = "achievements",
     "FE participation" = "participation",
-    "FE starts" = "starts"
+    "FE starts" = "starts",
+    "Qualified at Level 3 or above" ="level3AndAboveRate" 
   ),
   "Mismatch" = list("Supply vs demand TO COME" = "mismatch")
 )
