1083430fe85bfd62e8c6dd984c479ba1e31aee59
JAMES <Paul.JAMES@EDUCATION.GOV.UK>
2023-02-10 17:49:58 +0000

    remove shiny ref

--- global.R
+++ global.R
@@ -7,7 +7,7 @@
 # Library calls ---------------------------------------------------------------------------------
 shhh <- suppressPackageStartupMessages # It's a library, so shhh!
 
-shhh(library(shiny))
+#shhh(library(shiny))
 shhh(library(shinyjs))
 shhh(library(tools))
 shhh(library(testthat))
