cc75e481ca878b18d993390704eda080a9635ea6
Rich Bielby <richard.bielby@education.gov.uk>
2023-02-16 16:54:34 +0000

    Added some documentation comments on the cookie consent script and set expiry date for consent to 30 days

--- www/cookie-consent.js
+++ www/cookie-consent.js
@@ -1,13 +1,21 @@
+// Below is heavily lifted from the following link:
+// https://book.javascript-for-r.com/shiny-cookies.html 
+
+// Function to pass cookie info to Shiny
 function getCookies(){
   var res = Cookies.get();
   Shiny.setInputValue('cookies', res);
 }
 
+// Function to receive set cookie command info from Shiny - needed to record 
+// user's consent choice
 Shiny.addCustomMessageHandler('cookie-set', function(msg){
-  Cookies.set(msg.name, msg.value);
+  Cookies.set(msg.name, msg.value, { expires: 30 });
   getCookies();
 })
 
+// Function to receive remove cookie command info from Shiny - needed to cancel 
+// user's consent choice
 Shiny.addCustomMessageHandler('cookie-remove', function(msg){
   Cookies.remove(msg.name);
   getCookies();
@@ -17,6 +25,8 @@ $(document).on('shiny:connected', function(ev){
   getCookies();
 })
 
+// Function to set analytics consent dynamically - needed to fulfil user's 
+// consent choice
 Shiny.addCustomMessageHandler('analytics-consent', function(msg){
   gtag('consent', 'update', {
     'analytics_storage': msg.value
