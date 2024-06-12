db90e3d887810487f1e44cb4d12d054e7c78b41f
JAMES <Paul.JAMES@EDUCATION.GOV.UK>
2024-06-12 15:58:01 +0100

    correct the file path of some of the import scripts

--- importData/importEES.R
+++ importData/importEES.R
@@ -1,9 +1,9 @@
 ## 2.2 EES datasets----
 ### 2.2.1 Achievements by SSAt1, LAD, gender, level------------
-source("~/RProjects/lsip_dashboard/R/importFeSsa.R", echo=TRUE)
+source("~/RProjects/lsip_dashboard/importData/importFeSsa.R", echo=TRUE)
 
 ### 2.2.2 Achievements/starts/part by LAD and provision, level and age------------
-source("~/RProjects/lsip_dashboard/R/importFeAims.R", echo=TRUE)
+source("~/RProjects/lsip_dashboard/importData/importFeAims.R", echo=TRUE)
 
 ### 2.2.3 KS4 destinations----
-source("~/RProjects/lsip_dashboard/R/importDestinations.R", echo=TRUE)
+source("~/RProjects/lsip_dashboard/importData/importDestinations.R", echo=TRUE)
--- importData/importONS.R
+++ importData/importONS.R
@@ -1,6 +1,6 @@
 ## 2.3 ONS datasets ----
 ### 2.3.1 Business demography, UK ----
-source("~/RProjects/lsip_dashboard/R/importBusinessDemography.R", echo=TRUE)
+source("~/RProjects/lsip_dashboard/importData/importBusinessDemography.R", echo=TRUE)
 
 ### 2.3.2 ONS job adverts by SOC ----
-source("~/RProjects/lsip_dashboard/R/importJobAds.R", echo=TRUE)
+source("~/RProjects/lsip_dashboard/importData/importJobAds.R", echo=TRUE)
