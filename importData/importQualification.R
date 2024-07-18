62e44a2a904e8b4ff7cacb02d56f48403178f904
JAMES <Paul.JAMES@EDUCATION.GOV.UK>
2024-07-18 14:05:44 +0100

    UPdate aps data had to make a fix to get the correct periods for  qualifications data as it was triung to get the latest period but there is no data for that period add in tests to script

--- importData/importQualification.R
+++ importData/importQualification.R
@@ -7,8 +7,8 @@ cellsUseAps_qualNvq <- cellsListAps %>% filter(description.en %like% "T19:" & de
 I_qualAgeGenderNvq <- extractNomis("NM_17_1", "2019-12,2020-12,2021-12", cellsUseAps_qualNvq$id)
 # find cells we want in NVQ
 cellsUseAps_qualRqf <- cellsListAps %>% filter(description.en %like% "T19a:" & description.en %like% "Total")
-# get data
-I_qualAgeGenderRqf <- extractNomis("NM_17_1", "latestMINUS4,latest", cellsUseAps_qualRqf$id)
+# get rqf data from 2022 onwards. only jan-dec data avaialable so input of date in manual
+I_qualAgeGenderRqf <- extractNomis("NM_17_1", "2022-12,2023-12", cellsUseAps_qualRqf$id)
 
 ## Qualification level by age and gender ----
 C_qualAgeGender <- formatNomis(I_qualAgeGenderNvq %>%
