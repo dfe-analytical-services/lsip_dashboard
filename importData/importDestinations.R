9b60b0415c6a1bed90ffba19ef448239e1593e07
JAMES <Paul.JAMES@EDUCATION.GOV.UK>
2025-03-27 17:18:06 +0000

    Use the national level destinations data instead of summing from the LADs

--- importData/importDestinations.R
+++ importData/importDestinations.R
@@ -1,21 +1,28 @@
 ### 2.2.3 KS4 destinations----
-# National pupil database
+# National pupil database LA level
 folder <- "2-9_KS4destin"
 I_KS4 <- read.csv(file = paste0("./Data/", folder, "/", list.files(path = paste0("./Data/", folder))))
+# National pupil database National
+folder <- "2-9b_KS4destinNational"
+I_KS4National <- read.csv(file = paste0("./Data/", folder, "/", list.files(path = paste0("./Data/", folder))))
 
 ### 2.2.4 KS5 destinations----
 folder <- "2-10_KS5destin"
 I_KS5 <- read.csv(file = paste0("./Data/", folder, "/", list.files(path = paste0("./Data/", folder))))
+folder <- "2-10b_KS5destinNational"
+I_KS5National <- read.csv(file = paste0("./Data/", folder, "/", list.files(path = paste0("./Data/", folder))))
 
 # create all the other geographical areas based on LAs
 destinationsWithAreas <-
   # combine dataset
   bind_rows(
     I_KS4 %>% mutate(metric = "sustainedPositiveDestinationKS4Rate")%>%filter(institution_group=="State-funded mainstream schools"),#just schools to match the headline stats in the publication
-    I_KS5 %>% mutate(metric = "sustainedPositiveDestinationKS5Rate")%>%filter(institution_group=="State-funded mainstream schools & colleges",level_methodology=="16-18 Provider location")
+    I_KS4National %>% mutate(metric = "sustainedPositiveDestinationKS4Rate")%>%filter(institution_group=="State-funded mainstream schools",institution_type=="Total")%>%mutate_at(c('cohort','education','appren','all_work','all_notsust','all_unknown'), as.character)%>%select(-overall,-fe, -ssf,-sfc,-other_edu,-appl3,-appl2,-he),#just schools to match the headline stats in the publication
+    I_KS5 %>% mutate(metric = "sustainedPositiveDestinationKS5Rate")%>%filter(institution_group=="State-funded mainstream schools & colleges",level_methodology=="16-18 Provider location"),
+    I_KS5National %>% mutate(metric = "sustainedPositiveDestinationKS5Rate")%>%filter(institution_group=="State-funded mainstream schools & colleges",cohort_level=="Total")%>%mutate_at(c('cohort','education','appren','all_work','all_notsust','all_unknown'), as.character)%>%select(-overall,-fe, -ssf,-sfc,-other_edu,-appl3,-appl2,-he)
   ) %>%
   filter(data_type=="Number of pupils",breakdown_topic=="Total")%>%
-  select(-data_type, -institution_group, -institution_type, -version, -level_methodology, -old_la_code, -opportunity_area_code, -opportunity_area_name, -new_la_code, -la_name, -pcon_code, -pcon_name, -time_identifier, -country_code, -country_name, -region_code, -region_name, -breakdown_topic, -breakdown, -cohort_level,-overall, -fe, -ssf,-sfc,-other_edu,-appl3,-appl2,-appl4,-he,-fel1,-fel2,-fel3) %>% # remove unused columns
+  select(time_period,geographic_level,lad_code,lad_name,cohort,education,appren,all_work, all_notsust,all_unknown,metric,cohort_level_group) %>% # remove unused columns
   rename(areaCode = lad_code, area = lad_name, timePeriod = time_period) %>%
   # add dates
   mutate(chartPeriod = paste("AY", substr(timePeriod, 3, 4), "/", substr(timePeriod, 5, 6), sep = "")) %>%
@@ -30,13 +37,7 @@ destinationsWithAreas <-
   mutate_at(c('cohort','education','appren','all_work','all_notsust','all_unknown'), as.numeric)%>%#convert to numeric to sum
   addGeogs()
 
-# For destinations we need to also sum up for the whole country, so we take all LEPs again and relabel country ahead of the sum
-destinationsEngland <- destinationsWithAreas %>%
-  filter(stringr::str_sub(geogConcat, -3, -1) == "LEP") %>%
-  mutate(geogConcat = "England") %>%
-  bind_rows(destinationsWithAreas)#%>%
-
-groupedStats <- destinationsEngland %>%
+groupedStats <- destinationsWithAreas %>%
   filter(newArea == 1) %>% # no need to group national or LAs that haven't changed
   ungroup() %>%
   select(-newArea) %>%
@@ -47,7 +48,7 @@ groupedStats <- destinationsEngland %>%
 # add back on original LADUs and format
 C_destinations <- bind_rows(
   groupedStats,
-  destinationsEngland %>%
+  destinationsWithAreas %>%
     filter(newArea == 0)
 ) %>%
   select(-newArea) %>%
