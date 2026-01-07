212eed761050996f47c4e18309f30b0f2425894a
MEHTA <Meera.MEHTA@education.gov.uk>
2026-01-07 09:30:01 +0000

    Update to version control date

--- ui.R
+++ ui.R
@@ -762,7 +762,7 @@ Shiny.addCustomMessageHandler(\'updateActiveNav\', function(activeId) {
             div(
               class = "panel-body",
               h2("Latest update"),
-              p("xx xxx 202x (1.6.6)"),
+              p("07 Jan 2026 (1.6.6)"),
               tags$ul(
                 tags$li("Switch to new job adverts data."),
                 tags$li("Update to latest online job advert data.")
