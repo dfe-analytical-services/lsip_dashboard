97e1ba639c37d2fbeffce5ed158f3467ae548396
WEDGE-ROBERTS <Rebecca.WEDGE-ROBERTS@EDUCATION.GOV.UK>
2026-04-14 10:27:02 +0100

    changed update date

--- ui.R
+++ ui.R
@@ -796,7 +796,7 @@ Shiny.addCustomMessageHandler(\'updateActiveNav\', function(activeId) {
             div(
               class = "panel-body",
               h2("Latest update"),
-              p("13 Apr 2026 (1.6.11)"),
+              p("14 Apr 2026 (1.6.11)"),
               tags$ul(
                 tags$li("Addition of 'Job adverts (Pilot)' page."),
                 tags$li("Update to the latest online job advert data (Feb 2026)."),
