f6dad5cf99a69ba919cfcc54d27f42b1b598149e
JAMES <Paul.JAMES@EDUCATION.GOV.UK>
2026-05-21 09:52:04 +0100

    Change date

--- ui.R
+++ ui.R
@@ -822,7 +822,7 @@ Shiny.addCustomMessageHandler(\'updateActiveNav\', function(activeId) {
             div(
               class = "panel-body",
               h2("Latest update"),
-              p("15 May 2026 (1.6.12)"),
+              p("21 May 2026 (1.6.12)"),
               tags$ul(
                 tags$li("Update to latest APS employment data.")
               ),
