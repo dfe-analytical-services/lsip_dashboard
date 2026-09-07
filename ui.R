2f748fd945cd97ed16ed213626a31b394696114c
joescottse <joseph.scott2@dwp.gov.uk>
2026-09-07 14:05:15 +0100

    Fix nesting of brackets on the change log

--- ui.R
+++ ui.R
@@ -826,20 +826,20 @@ Shiny.addCustomMessageHandler(\'updateActiveNav\', function(activeId) {
               details(
                 label = "Previous updates",
                 inputId = "PreviousUpdate",
-                p("2 Sep 2026 (1.6.18)"),
-                tags$ul(
-                  tags$li("Change base map.")
-                ),
-                p("(1.6.17)"),
-                tags$ul(
-                  tags$li("Add CA websites.")
-                ),
-                p("30 Jul 2026 (1.6.16)"),
-                tags$ul(
-                  tags$li("Update to July 2026 online job advert data."),
-                  tags$li("Add LSIP websites.")
-                ),
                 p(
+                  p("2 Sep 2026 (1.6.18)"),
+                  tags$ul(
+                    tags$li("Change base map.")
+                  ),
+                  p("(1.6.17)"),
+                  tags$ul(
+                    tags$li("Add CA websites.")
+                  ),
+                  p("30 Jul 2026 (1.6.16)"),
+                  tags$ul(
+                    tags$li("Update to July 2026 online job advert data."),
+                    tags$li("Add LSIP websites.")
+                  ),
                   p("22 Jul 2026 (1.6.15)"),
                   tags$ul(
                     tags$li("Add grouped geography feature to Datahub to allow the download of data combined over multiple, user chosen areas."),
